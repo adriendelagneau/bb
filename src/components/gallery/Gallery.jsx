0dea9e6a5f61f8b4900b97b5eddb4df63a90c678
adriendelagneau <delagneauadrien@yahoo.fr>
2023-11-14 10:03:31 +0100

    first commit

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -7,51 +7,51 @@ gsap.registerPlugin(ScrollTrigger);
 
 const Gallery = () => {
 
-    const sectionRef = useRef(null);
+  const sectionRef = useRef(null);
 
-    useLayoutEffect(() => {
-      // GSAP animation setup
-      const section = sectionRef.current;
-  
-      let ctx = gsap.context(() => {
-              // pin the section
-            gsap.to(section, {
-              scrollTrigger: {
-                trigger: section,
-                start: 'top top',
-                end: () => `+=${section.clientHeight}`,
-                pin: true,
-              scrub: true,
-              },
-            })
-              
-              gsap.timeline({
-                  scrollTrigger: {
-                      trigger: section,
-                      start: 'top top',
-                      end: () => `+=${section.clientHeight}`,
-                      scrub: true,
-                    },
-              })
-              .to('.content1', {clipPath: "polygon(0% 0%, 0% 0%, 0% 100%, 0% 100%)"})
-      }, section);
+  useLayoutEffect(() => {
+    // GSAP animation setup
+    const section = sectionRef.current;
 
-      return () => ctx.revert();
+    let ctx = gsap.context(() => {
 
-    }, []);
+      // pin the section
+      gsap.to(section, {
+        scrollTrigger: {
+          trigger: section,
+          start: 'top top',
+          end: () => `+=${section.clientHeight}`,
+          pin: true,
+          scrub: true,
+        },
+      })
+
+      // anime clip path
+      gsap.timeline({
+        scrollTrigger: {
+          trigger: section,
+          start: 'top top',
+          end: () => `+=${section.clientHeight}`,
+          scrub: true,
+        },
+      })
+        .to('.content1', { clipPath: "polygon(0% 0%, 0% 0%, 0% 100%, 0% 100%)" })
+
+    }, section);
+
+    return () => ctx.revert();
+
+  }, []);
 
   return (
     <div id='gallery' ref={sectionRef}>
       <div className="container">
-
-          <div className="content1">
-              <img src='../../../public/290628beaf1d7add6159b0c0730a9ff8b21040ae4585fde305a5ca7e75494ed6.webp' alt='' />
+        <div className="content1">
+          <img src='../../../public/290628beaf1d7add6159b0c0730a9ff8b21040ae4585fde305a5ca7e75494ed6.webp' alt='' />
         </div>
-        
         <div className="content2">
-              <img src='../../../public/istockphoto-515173623-612x612.jpg' alt='' />
+          <img src='../../../public/istockphoto-515173623-612x612.jpg' alt='' />
         </div>
-
       </div>
     </div>
   )
